7b5ede8e11c71acea1aecc7d9cacf9969564c2b6
Bryan <bkim2001@gmail.com>
2021-10-07 14:42:29 -0400

    move firestore instance

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from flaskr import create_app
-import firebase_admin
-from firebase_admin import credentials
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
 app = create_app()
 
 if __name__ == '__main__':
