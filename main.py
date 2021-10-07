1625f2d4aeb36e0a5783596acbaa7329539d2e3e
Bryan <bkim2001@gmail.com>
2021-10-07 13:22:19 -0400

    add blueprints

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flaskr import create_app
+import firebase_admin
+from firebase_admin import credentials
 
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 app = create_app()
 
 if __name__ == '__main__':
