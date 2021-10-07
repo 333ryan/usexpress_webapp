2771ef8f0960532577db95b73fdb23885d0474fa
Bryan <bkim2001@gmail.com>
2021-10-07 14:43:07 -0400

    add firestore test route

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
+from flaskr.firestore import firestore_db
 
 views = Blueprint('views', __name__)
+user_collection = firestore_db.collection("users")
 
 
 @views.route('/')
 def start():
     return render_template("home.html")
+
+
+@views.route('/firestore')
+def firestore():
+    doc_ref = user_collection.document("n8tegreoHLQQp2pkL3UkURy8EBf2").get()
+    return f"User Data: {doc_ref.to_dict()}"
